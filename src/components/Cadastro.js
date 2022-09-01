a5bbc940bf9b3e73796775bb1d7934bd1427c6c7
Fernando Ruiz <fernandorpx27@gmail.com>
2022-09-01 16:58:49 -0300

    tela de login e cadastro melhoradas

--- src/components/Cadastro.js
+++ src/components/Cadastro.js
@@ -1,14 +1,22 @@
 import React from "react";
-import Menu from './Menu'
 
 const Cadastro = () => {
     return(
         <div>
         <h2>Pagina de Cadastro</h2>
-        <li> <input className="inputCadastroNome" placeholder="Nome" /> </li>
+        <br></br>
+        <h3>Email</h3>
+        <br></br>
+        <li> <input className="inputCadastroEmail" placeholder="Email" /> </li>
+        <h3>Senha</h3>
         <li> <input className="inputCastroSenha" placeholder="Senha" /> </li>
+        <h3>Confirme sua senha</h3>
         <li> <input className="inputCadastroConfirma" placeholder="Confirma Senha" /> </li>
-        < Menu />
+        <br></br>
+        <a href="/login"> <button>Cadastrar</button> </a>
+        <br></br>
+        <br></br>
+        <li> <a href="/login">Ja possuo conta</a> </li> 
         </div>
     )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,21 @@
 import React from "react";
-import Menu from './Menu'
+
 
 const Login = () => {
     return(
         <div>
         <h2>Pagina de Login</h2>
-        <li> <input className="inputLoginoNome" placeholder="Nome" /> </li>
+        <br></br>
+        <h3>Email</h3>
+        <li> <input className="inputLoginoEmail" placeholder="Email" /> </li>
+        <h3>Senha</h3>
         <li> <input className="inputLoginSenha" placeholder="Senha" /> </li>
-        < Menu />
+        <br></br>
+        <a>Esqueceu a senha?</a>
+        <br></br> <br></br>
+        <a href="/"> <button>Entrar</button> </a>
+        <br></br> <br></br>
+        <li> <a href="/cadastro">Não possui conta? Cadastre-se aqui</a></li>
         </div>
     )
 }
