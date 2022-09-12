88cbc3adb6c1097d1e01223004e009291734ee59
Fernando Ruiz <fernandorpx27@gmail.com>
2022-09-12 01:45:51 -0300

    SA finalizada

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Menu from './Menu'
+import Header from "./Header";
 
 const Contato = () => {
     return(
-        <div>
-            <h2>Contato</h2>
-            <Menu />
+        <div className="principal">
+            <Header />
+            <h3 className="textoContato1">Gestacy é um site focado no apoio a gestante desde os primeiros meses ate o momento do parto, oferecendo informaçoes e ajudando na organizaçao de compromissos. <br></br> Nós da Gestacy percebemos um aumento enorme na procura por auxilio durante o periodo de gravidez mas muitas não sabiam o que fazer e nem como se organizar perante ao periodo que estava passando.</h3>
+            <div className="divContato">
+            <img
+              className="imgContato"
+              src={require("./Home/imgs/logo.png")}
+              alt="imagem 1 carrossel"
+            />
+            </div>
         </div>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,20 @@ import React from "react";
 
 const Footer = () => {
     return(
-        <footer>
+        <footer className="footer1">
             <ul className="ListFooter">
-                <li className="ItemFooter"> <a href="/">Contato</a> </li>
-                <li></li>
+                <li className="ItemFooter">Contatos</li>
+                <li className="ItemFooter"> <a href="/contato" className="linkFooter">Sobre nós</a></li>
+                <li className="ItemFooter">Instagram</li>
+                <li className="ItemFooter">Facebook</li>
+            </ul>
+            <ul className="ListFooter2">
+                <li className="ItemFooter">Agende uma consulta</li>
+                <li className="ItemFooter"> <a href="/perfil" className="linkFooter">Acesse seu perfil</a></li>
+                <li className="ItemFooter">Fique atenta ao calendario</li>
+                <li className="ItemFooter"><a href="/" className="linkFooter">Voltar para a parte de cima do site</a></li>
             </ul>
         </footer>
     )
 }
-
 export default Footer;
